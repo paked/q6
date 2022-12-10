f0eafd75cca1752fb1511dd2fb4234314283164e
Harrison Shoebridge <harrison@theshoebridges.com>
2022-12-10 23:44:59 +1100

    Add basic working code editor
    Kinda based off of the ideas here (https://marijnhaverbeke.nl/blog/browser-input-reading.html
    ), ie. a hidden 'textarea' where input events go to, while the resulting
    'code' is rendered separately. This implementation is made much simpler
    by the fact that it doesn't need to handle large files, so the entirety
    of the document can stay in the textarea.
    
    Seems to work well enough, but I'm not sure it's what I
    need--considering we're going to need to be able to mix HTML elements
    into the source code. Although I'm also not sure that the pure
    contenteditable route is the right path either!

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # q6
 
-a programming language testing sandbox.
+a programming language testing sandbox for creative code.
+
+Current ideas:
+
+1. semantic code editing
+2. time travel debugging
+3. intelligent if-statements
+
+## semantic code editing
+
+Part of the reason Scratch seems to work as an environment for teaching novice programmers how to code is that it utilises a block-based drag-and-drop interface to minimise cognitive load. Could a similar abstraction be created which operates on top of the "raw text" model, instead of replacing it entirely? Would this make it more appetising to people who want to learn a "real" programming language (ignoring if this sentiment is useful or not)?
+
+Goals:
+
+1. discoverability of funcionality
+2. make it clearer what
+
+### features
+
+- structural expansion: things like function declarations, if statements, while statements, assignments, and for loops are structurally represented in the interface (ie. you don't have to do any formatting). 
+- command palette
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,128 @@
 import './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<div contenteditable>
-<p>hello world</p>
-</div>
-`
+interface Point {
+    row: number;
+    col: number;
+}
 
-// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+interface Selection {
+    start: Point;
+    end: Point;
+}
+
+const display = document.querySelector<HTMLDivElement>('#display')!;
+const editor = document.querySelector<HTMLTextAreaElement>('#editor')!;
+const measure = document.querySelector<HTMLSpanElement>('#measure')!;
+
+const cursor = document.querySelector<HTMLSpanElement>('#cursor')!;
+
+let ourSelection: Selection;
+
+function getLineNum(elem: HTMLElement): number | undefined {
+    if (elem.hasAttribute('data-linenum')) {
+        return parseInt(elem.getAttribute('data-linenum')!);
+    }
+
+    if (!elem.parentElement) {
+        return;
+    }
+
+    return getLineNum(elem.parentElement)
+}
+
+display.addEventListener('click', () => {
+    editor.focus();
+})
+
+display.addEventListener('mouseup', (ev: MouseEvent) => {
+    let selection = document.getSelection();
+
+    ourSelection = {
+        start: {
+            row: getLineNum(document.getSelection()?.anchorNode?.parentElement!)!,
+            col: selection?.anchorOffset!,
+        },
+        end: {
+            row: getLineNum(document.getSelection()?.focusNode?.parentElement!)!,
+            col: selection?.focusOffset!,
+        }
+    }
+
+    const lines = editor.value.split('\n');
+
+    editor.selectionStart = pointToIndex(lines, ourSelection.start);
+    editor.selectionEnd = pointToIndex(lines, ourSelection.end);
+});
+
+function indexToPoint(lines: string[], index: number): Point {
+    let col = index;
+    let row = 0;
+
+    for (let i = 0; i < lines.length; i++) {
+        let line = lines[i];
+
+        if (col < line.length + 1) {
+            break;
+        }
+
+        col -= line.length + 1;
+
+        row += 1;
+    }
+
+    return {
+        row, col
+    }
+}
+
+function pointToIndex(lines: string[], point: Point): number {
+    let index = 0;
+
+    for (let i = 0; i < lines.length; i++) {
+        if (i != point.row) {
+            index += lines[i].length + 1;
+
+            continue
+        }
+
+        break;
+    }
+
+    index += point.col;
+
+    return index;
+}
+
+window.redrawTextArea = function() {
+    const lines = editor.value.split('\n');
+
+    let { row, col } = indexToPoint(lines, editor.selectionStart);
+
+    measure.innerText = 'q'.repeat(col + 1);
+
+    document.body.appendChild(cursor);
+    display.innerHTML = lines
+        .map(line => {
+            if (line.length === 0) {
+                line = "&nbsp;"
+            }
+
+            return `<span><pre> ${line}</pre></span>`;
+        })
+        .map((line, i) => {
+            return `<div class="line" data-linenum="${i}">${line}</div>`;
+        })
+        .join('\n');
+
+    let focusRow = display.children[row] as HTMLElement;
+
+    focusRow.prepend(cursor);
+
+    let xoffset = measure.getBoundingClientRect().width;
+    let yoffset = 0;
+    cursor.style.transform = `translate(${xoffset}px, ${yoffset}px)`;
+}
+
+editor.addEventListener('selectionchange', window.redrawTextArea);
+
+window.redrawTextArea();
