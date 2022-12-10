fb906e2e16646ea4de1ce8c91305bf1d427ae505
Harrison Shoebridge <harrison@theshoebridges.com>
2022-12-11 00:34:10 +1100

    Add non-granular visualised selections
    If part of a line is in the selection, all of it is shown as highlighted.

--- src/main.ts
+++ src/main.ts
@@ -10,49 +10,28 @@ interface Selection {
     end: Point;
 }
 
-const display = document.querySelector<HTMLDivElement>('#display')!;
-const editor = document.querySelector<HTMLTextAreaElement>('#editor')!;
-const measure = document.querySelector<HTMLSpanElement>('#measure')!;
-
-const cursor = document.querySelector<HTMLSpanElement>('#cursor')!;
-
-let ourSelection: Selection;
-
-function getLineNum(elem: HTMLElement): number | undefined {
-    if (elem.hasAttribute('data-linenum')) {
-        return parseInt(elem.getAttribute('data-linenum')!);
+function selectionShouldNormalize(sel: Selection): boolean {
+    if (sel.end.row < sel.start.row) {
+        return true;
+    }
+    
+    if (sel.end.col < sel.start.col) {
+        return true;
     }
 
-    if (!elem.parentElement) {
-        return;
-    }
-
-    return getLineNum(elem.parentElement)
+    return false;
 }
 
-display.addEventListener('click', () => {
-    editor.focus();
-})
-
-display.addEventListener('mouseup', (ev: MouseEvent) => {
-    let selection = document.getSelection();
-
-    ourSelection = {
-        start: {
-            row: getLineNum(document.getSelection()?.anchorNode?.parentElement!)!,
-            col: selection?.anchorOffset!,
-        },
-        end: {
-            row: getLineNum(document.getSelection()?.focusNode?.parentElement!)!,
-            col: selection?.focusOffset!,
+function selectionNormalize(sel: Selection): Selection {
+    if (selectionShouldNormalize(sel)) {
+        return {
+            start: sel.end,
+            end: sel.start,
         }
     }
 
-    const lines = editor.value.split('\n');
-
-    editor.selectionStart = pointToIndex(lines, ourSelection.start);
-    editor.selectionEnd = pointToIndex(lines, ourSelection.end);
-});
+    return sel;
+}
 
 function indexToPoint(lines: string[], index: number): Point {
     let col = index;
@@ -93,36 +72,109 @@ function pointToIndex(lines: string[], point: Point): number {
     return index;
 }
 
-window.redrawTextArea = function() {
+function getLineNum(elem: HTMLElement): number | undefined {
+    if (elem.hasAttribute('data-linenum')) {
+        return parseInt(elem.getAttribute('data-linenum')!);
+    }
+
+    if (!elem.parentElement) {
+        return;
+    }
+
+    return getLineNum(elem.parentElement)
+}
+
+const display = document.querySelector<HTMLDivElement>('#display')!;
+const editor = document.querySelector<HTMLTextAreaElement>('#editor')!;
+const measure = document.querySelector<HTMLSpanElement>('#measure')!;
+const cursor = document.querySelector<HTMLSpanElement>('#cursor')!;
+
+let ourSelection: Selection = {
+    start: {
+        row: 0,
+        col: 0,
+    },
+    end: {
+        row: 0,
+        col: 0,
+    }
+};
+
+display.addEventListener('click', () => {
+    editor.focus();
+})
+
+display.addEventListener('mouseup', (_: MouseEvent) => {
+    let selection = document.getSelection();
+
+    ourSelection = {
+        start: {
+            row: getLineNum(document.getSelection()?.anchorNode?.parentElement!)!,
+            col: selection?.anchorOffset!,
+        },
+        end: {
+            row: getLineNum(document.getSelection()?.focusNode?.parentElement!)!,
+            col: selection?.focusOffset!,
+        }
+    }
+
+    ourSelection = selectionNormalize(ourSelection);
+
     const lines = editor.value.split('\n');
 
-    let { row, col } = indexToPoint(lines, editor.selectionStart);
+    editor.selectionStart = pointToIndex(lines, ourSelection.start);
+    editor.selectionEnd = pointToIndex(lines, ourSelection.end);
+});
 
-    measure.innerText = 'q'.repeat(col + 1);
+window.redrawTextArea = function() {
+    // TODO(harrison): we should store this somewhere global whenever an editor changes, then it doesn't have to be pulled ever redraw.
+    const lines = editor.value.split('\n');
 
+    let start = ourSelection.start;
+    let end = ourSelection.end;
+
+    // NOTE(harrison): move cursor out of code area so we can refresh that.
     document.body.appendChild(cursor);
+
     display.innerHTML = lines
-        .map(line => {
+        .map((line, i) => {
+            // TODO(harrison): handle partially highlighted rows
+            let highlighted = false;
+            if ((start.row !== end.row || start.col !== end.col) && (i >= start.row && i <= end.row)) {
+                highlighted = true;
+            }
+
             if (line.length === 0) {
                 line = "&nbsp;"
             }
+            
+            const highlightedClass = highlighted ? 'highlighted' : '';
 
-            return `<span><pre> ${line}</pre></span>`;
+            return `<span class="${highlightedClass}"><pre>${line}</pre></span>`;
         })
         .map((line, i) => {
             return `<div class="line" data-linenum="${i}">${line}</div>`;
         })
         .join('\n');
 
-    let focusRow = display.children[row] as HTMLElement;
-
+    let focusRow = display.children[start.row] as HTMLElement;
+    // NOTE(harrison): move cursor to current line
     focusRow.prepend(cursor);
 
+    measure.innerText = 'q'.repeat(start.col);
     let xoffset = measure.getBoundingClientRect().width;
-    let yoffset = 0;
-    cursor.style.transform = `translate(${xoffset}px, ${yoffset}px)`;
+    cursor.style.transform = `translateX(${xoffset}px)`;
 }
 
-editor.addEventListener('selectionchange', window.redrawTextArea);
+editor.addEventListener('selectionchange', () => {
+    const lines = editor.value.split('\n');
+
+    ourSelection.start = indexToPoint(lines, editor.selectionStart);
+    ourSelection.end = indexToPoint(lines, editor.selectionEnd);
+
+    ourSelection = selectionNormalize(ourSelection);
+
+    window.redrawTextArea()
+});
 
 window.redrawTextArea();
